706624797e3c138e810b14fb311da34ee9155ec8
wang xiaozai <wxy0110@ruc.edu.cn>
2023-12-08 02:31:52 +0800

    更新功能和演员表

--- app.py
+++ app.py
@@ -42,6 +42,13 @@ class Movie(db.Model): # 表名将会是 movie
     type = db.Column(db.String(120))#电影类型
     box = db.Column(db.String(50))#电影票房
 
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(240))  # 姓名
+    gender = db.Column(db.String(20))  # 性别
+    country = db.Column(db.String(120))  # 国家
+
+
 import click
 @app.cli.command()
 @click.option('--username', prompt=True, help='The username usedto login.')
@@ -78,7 +85,7 @@ import click
 def forge():
     """Generate fake data."""
     db.create_all()
-# 全局的两个变量移动到这个函数内
+# 全局的变量移动到这个函数内
     name = '旺小仔'
     movies = [
     {'title': '战狼2', 'year': '2017', 'country': '中国', 'type': '战争', 'box': '56.84亿'},
@@ -99,12 +106,83 @@ def forge():
     {'title': '你好，李焕英', 'year': '2021', 'country': '中国', 'type': '喜剧', 'box': '54.13亿'},
     {'title': '长津湖', 'year': '2021', 'country': '中国', 'type': '战争', 'box': '53.48亿'},
     {'title': '速度与激情9', 'year': '2021', 'country': '美国', 'type': '动作', 'box': '13.92亿'},
+    {'title': '霍元甲', 'year': '2006', 'country': '中国', 'type': '动作', 'box': '1.76亿'},
+    {'title': '战狼', 'year': '2015', 'country': '中国', 'type': '战争', 'box': '8.43亿'},
+    {'title': '红海行动', 'year': '2018', 'country': '中国', 'type': '战争', 'box': '36.5亿'},
+    {'title': '唐人街探案', 'year': '2015', 'country': '中国', 'type': '喜剧', 'box': '12.8亿'},
+    {'title': '大话西游之大圣娶亲', 'year': '1995', 'country': '香港', 'type': '喜剧', 'box': '7.8亿'},
+        {'title': '少年派的奇幻漂流', 'year': '2012', 'country': '美国', 'type': '冒险', 'box': '9.01亿'},
+        {'title': '完美陌生人', 'year': '2016', 'country': '意大利', 'type': '喜剧', 'box': '8.86亿'},
+        {'title': '钢琴师', 'year': '2002', 'country': '法国', 'type': '剧情', 'box': '2.65亿'},
+        {'title': '心灵捕手', 'year': '1998', 'country': '美国', 'type': '剧情', 'box': '3.21亿'},
+        {'title': '疯狂的石头', 'year': '2006', 'country': '中国', 'type': '喜剧', 'box': '1.61亿'},
+        {'title': '当幸福来敲门', 'year': '2006', 'country': '美国', 'type': '剧情', 'box': '8.3亿'},
+        {'title': '怦然心动', 'year': '2010', 'country': '美国', 'type': '喜剧', 'box': '4.68亿'},
+        {'title': '摔跤吧！爸爸', 'year': '2016', 'country': '印度', 'type': '剧情', 'box': '4.37亿'},
+        {'title': '放牛班的春天', 'year': '2004', 'country': '法国', 'type': '剧情', 'box': '3.05亿'},
+        {'title': '乱世佳人', 'year': '1939', 'country': '美国', 'type': '剧情', 'box': '39.2亿'},
+        {'title': '千与千寻', 'year': '2001', 'country': '日本', 'type': '动画', 'box': '25.03亿'},
+        {'title': '教父', 'year': '1972', 'country': '美国', 'type': '剧情', 'box': '2.46亿'},
+        {'title': '指环王：王者归来', 'year': '2003', 'country': '美国', 'type': '奇幻', 'box': '7.57亿'},
+        {'title': '阿甘正传', 'year': '1994', 'country': '美国', 'type': '剧情', 'box': '6.71亿'},
+        {'title': '泰坦尼克号', 'year': '1997', 'country': '美国', 'type': '剧情', 'box': '19.76亿'},
+        {'title': '盗梦空间', 'year': '2010', 'country': '美国', 'type': '科幻', 'box': '14.68亿'},
+        {'title': '楚门的世界', 'year': '1998', 'country': '美国', 'type': '剧情', 'box': '6.94亿'},
+        {'title': '海上钢琴师', 'year': '1998', 'country': '意大利', 'type': '剧情', 'box': '2.4亿'},
+        {'title': '机器人总动员', 'year': '2008', 'country': '美国', 'type': '动画', 'box': '8.53亿'},
+        {'title': 'V字仇杀队', 'year': '2005', 'country': '美国', 'type': '动作', 'box': '1.33亿'},
+        {'title': '西游伏妖篇', 'year': '2017', 'country': '中国', 'type': '奇幻', 'box': '16.8亿'},
+        {'title': '寻龙诀', 'year': '2015', 'country': '中国', 'type': '奇幻', 'box': '16.75亿'},
+        {'title': '战狼', 'year': '2015', 'country': '中国', 'type': '战争', 'box': '14.72亿'},
+        {'title': '碟中谍5：神秘国度', 'year': '2015', 'country': '美国', 'type': '动作', 'box': '13.86亿'},
+        {'title': '建军大业', 'year': '2017', 'country': '中国', 'type': '历史', 'box': '13.62亿'},
+        {'title': '疯狂动物城', 'year': '2016', 'country': '美国', 'type': '动画', 'box': '12.68亿'},
+        {'title': '卧底巨星', 'year': '2017', 'country': '中国', 'type': '喜剧', 'box': '12.6亿'},
     ]
+
+    players = [
+        {'name':'吴京','gender':'男','country':'中国'},
+        {'name': '饺子', 'gender': '男', 'country': '中国'},
+        {'name': '屈楚萧', 'gender': '男', 'country': '中国'},
+        {'name': '郭帆', 'gender': '男', 'country': '中国'},
+        {'name': '乔罗素', 'gender': '男', 'country': '美国'},
+        {"name": "小罗伯特·唐尼", "gender": "男", "country": "美国"},
+        {"name": "克里斯·埃文斯", "gender": "男", "country": "美国"},
+        {"name": "林超贤", "gender": "男", "country": "中国"},
+        {"name": "张译", "gender": "男", "country": "中国"},
+        {"name": "黄景瑜", "gender": "男", "country": "中国"},
+        {"name": "陈思诚", "gender": "男", "country": "中国"},
+        {"name": "王宝强", "gender": "男", "country": "中国"},
+        {"name": "刘昊然", "gender": "男", "country": "中国"},
+        {"name": "文牧野", "gender": "男", "country": "中国"},
+        {"name": "徐峥", "gender": "男", "country": "中国"},
+        {"name": "刘伟强", "gender": "男", "country": "中国"},
+        {"name": "张涵予", "gender": "男", "country": "中国"},
+        {"name": "F·加里·格雷", "gender": "男", "country": "美国"},
+        {"name": "范·迪塞尔", "gender": "男", "country": "美国"},
+        {"name": "杰森·斯坦森", "gender": "男", "country": "美国"},
+        {"name": "闫非", "gender": "男", "country": "中国"},
+        {"name": "沈腾", "gender": "男", "country": "中国"},
+        {"name": "安东尼·罗素", "gender": "男", "country": "美国"},
+        {"name": "克里斯·海姆斯沃斯", "gender": "男", "country": "美国"},
+        {"name": "许诚毅", "gender": "男", "country": "中国"},
+        {"name": "梁朝伟", "gender": "男", "country": "中国"},
+        {"name": "白百何", "gender": "女", "country": "中国"},
+        {"name": "井柏然", "gender": "男", "country": "中国"},
+        {"name": "管虎", "gender": "男", "country": "中国"},
+        {"name": "王千源", "gender": "男", "country": "中国"},
+        {"name": "姜武", "gender": "男", "country": "中国"},
+        {"name": "宁浩", "gender": "男", "country": "中国"},
+    ]
+
     user = User(name=name)
     db.session.add(user)
     for m in movies:
         movie = Movie(title=m['title'], year=m['year'], country=m['country'], type=m['type'],box=m['box'])
         db.session.add(movie)
+    for m in players:
+        player = Player(name=m['name'], gender=m['gender'], country=m['country'])
+        db.session.add(player)
     db.session.commit()
     click.echo('Done.')
 
@@ -230,8 +308,65 @@ def delete(movie_id):
     flash('Item deleted.')
     return redirect(url_for('index')) # 重定向回主页
 
+@app.route('/search', methods=['POST'])
+def search():
+    movies = Movie.query.all()
+    search_term = request.form['searchTerm'].lower()
+    results = [movie for movie in movies if search_term in movie.title.lower()]
+    return render_template('index.html', movies=results)
 
+@app.route('/cate', methods=['POST'])
+def cate():
+    movies = Movie.query.all()
+    search_term = request.form['searchTerm'].lower()
+    results = [movie for movie in movies if search_term in movie.type.lower()]
+    return render_template('index.html', movies=results)
 
+@app.route('/act', methods=['GET', 'POST'])
+def act():
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        name = request.form.get('name')  # 传入表单对应输入字段的name 值
+        gender = request.form.get('gender')
+        country = request.form.get('country')
 
+        # 验证数据
+        if not name or not gender or not country:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('act'))  # 重定向回主页
+        # 保存表单数据到数据库
+        player = Player(name=name, gender=gender, country=country)  # 创建记录
+        db.session.add(player)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('act'))  # 重定向回主页
+    user = User.query.first()
+    players = Player.query.all()
+    return render_template('actor.html', user=user, players=players)
 
+@app.route('/player/deleteact/<int:player_id>', methods=['POST']) #限定只接受 POST 请求
+def deleteact(player_id):
+    player = Player.query.get_or_404(player_id) # 获取电影记录
+    db.session.delete(player) # 删除对应的记录
+    db.session.commit() # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('act')) # 重定向回主页
 
+@app.route('/player/editact/<int:player_id>', methods=['GET', 'POST'])
+def editact(player_id):
+    player = Player.query.get_or_404(player_id)
+    if request.method == 'POST': # 处理编辑表单的提交请求
+        name = request.form['name']
+        gender = request.form['gender']
+        country = request.form['country']
+        if not name or not gender or not country:
+            flash('Invalid input.')
+            return redirect(url_for('editact', player_id=player_id))
+# 重定向回对应的编辑页面
+        player.name = name # 更新标题
+        player.gender = gender # 更新年份
+        player.country = country  # 更新国家
+        db.session.commit() # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('act')) # 重定向回主页
+    return render_template('editact.html', player=player) # 传入被编辑的电影记录
